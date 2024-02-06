1bf800ff6a7515a34cd7f2c8357c3f631d2de52a
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2024-02-06 21:32:39 +0000

    shadcn&hero

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
-import { SignedOut } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -10,10 +12,22 @@ const Header = () => {
             <Link href='/' className="w-36">
                 <Image src='/assets/images/logo.svg' width={128} height={38} alt="logo" />
             </Link>
+
+            <SignedIn>
+                <nav className="md:flex-between hidden w-full max-w-xs">
+                    <NavItems />
+                </nav>
+            </SignedIn>
+
             <div className="flex w-32 justify-end gap-3">
+                <SignedIn>
+                   <UserButton afterSignOutUrl="/" />
+                   <MobileNav />
+                </SignedIn>
+
                 <SignedOut>
                     <Button asChild className="rounded-full" size='lg'>
-                        <Link href='/sing-in'>Login</Link>
+                        <Link href='/sign-in'>Login</Link>
                     </Button>
                 </SignedOut>
             </div>
